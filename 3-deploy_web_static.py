afd13cfa768c4ca988e8ee38e8db6875b4ef2c25
kevin-kirui <kipkoechkevin42@gmail.com>
2024-05-28 13:57:50 -0700

    -a

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -20,7 +20,8 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except Exception as e:
+        print(e)
         return None
 
 
@@ -41,7 +42,8 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
